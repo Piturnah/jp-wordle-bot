7a489121f99cfb368f1d00f7ed184ef330fefa7c
Janosch Reppnow <janoschre@gmail.com>
2022-01-31 21:53:46 +0100

    intermittent version to merge

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,78 @@
 require("dotenv").config();
 
-const { Client, Intents } = require("discord.js");
+import { Client, Intents, Message, Snowflake } from "discord.js";
+import { Game, CharState, State } from "./interfaces";
+import { COMMANDS } from "./commands";
+
+class Bot {
+	client: Client;
+
+	activeGames = new Map<Snowflake, Game>();
+
+	constructor(client: Client) {
+		this.client = client;
+	}
+
+	start(token: string | undefined) {
+		client.once("ready", this.ready);
+		client.on("messageCreate", this.messageCreate);
+		client.login(token);
+	}
+	ready() {
+		if (null !== client.user)
+			console.log(
+				`${
+					client.user.tag
+				} successfully logged in at ${new Date().toTimeString()}`
+			);
+	}
+
+	messageCreate(message: Message) {
+		let userId = message.author.id;
+		let activeGame = this.activeGames.get(message.channelId);
+		if (undefined !== activeGame) {
+			switch (activeGame.state()) {
+				case State.Setup:
+					if (
+						message.content.startsWith(
+							COMMANDS.JOIN
+						)
+					) {
+						activeGame.join(userId);
+					} else if (
+						message.content.startsWith(
+							COMMANDS.START
+						)
+					) {
+						if (activeGame.start(userId)) {
+						}
+					}
+					break;
+				case State.Running:
+					this.handleResponse(
+						activeGame.makeGuess(
+							userId,
+							message.content
+						)
+					);
+					break;
+			}
+		} else {
+		}
+	}
+
+	handleResponse(guessResult: boolean | CharState[]) {}
+
+	prompt() {
+		// TODO
+	}
+}
 
 // https://discord.com/developers/docs/topics/gateway#gateway-intents
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-client.once("ready", () => {
-	console.log(`${client.user.tag} successfully logged in at ${new Date().toTimeString()}`)
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.login(process.env.DISCORD_TOKEN);
+let bot = new Bot(client);
+
+bot.start(process.env.DISCORD_TOKEN);
--- src/commands.ts
+++ src/commands.ts
@@ -1,3 +1,4 @@
 export const enum COMMANDS {
 	JOIN = "!join",
+	START = "!start",
 }
