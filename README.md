1dd74a09f8220268ced32b36a54987633aca154f
Piturnah <peterhebden6@gmail.com>
2022-01-31 13:30:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Getting started
+
+### Prerequisites
+
+* [Node.JS](https://nodejs.org/)
+
+### Setting up
+
+1. Install node modules
+```
+npm install
+```
+
+2. Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+3. Compiles for production
+```
+npm run build
+```
---
+++ src/bot.ts
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const { Client, Intents } = require("discord.js");
+
+// https://discord.com/developers/docs/topics/gateway#gateway-intents
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once("ready", () => {
+	console.log(`${client.user.tag} successfully logged in at ${new Date().toTimeString()}`)
+});
+
+client.login(process.env.DISCORD_TOKEN);
