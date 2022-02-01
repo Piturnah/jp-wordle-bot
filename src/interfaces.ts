a8c26336682983ac146701f04bd6eb5b597dc828
Janosch Reppnow <janoschre@gmail.com>
2022-02-01 22:35:45 +0100

    made image dimensions configurable in renderer, increased default dimensions

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,57 +1,86 @@
 import { Snowflake } from "discord.js";
 
 export enum SpecialTurnResponse {
-	WrongPlayer,
-	WonGame,
-	BadGuess,
+    WrongPlayer,
+    WonGame,
+    BadGuess,
 }
 
 export enum CharState {
-	Wrong,
-	Moved,
-	Correct,
+    Wrong,
+    Moved,
+    Correct,
 }
 
 export enum State {
-	Setup,
-	Running,
+    Setup,
+    Running,
 }
 
 export interface Game {
-	getState(): State;
-	join(player: Snowflake): boolean;
-	start(player: Snowflake): boolean;
-	nextGuessExpectedFrom(): Snowflake;
-	makeGuess(
-		player: Snowflake,
-		guess: string
-	): SpecialTurnResponse | CharState[];
+    getState(): State;
+    join(player: Snowflake): boolean;
+    start(player: Snowflake): boolean;
+    nextGuessExpectedFrom(): Snowflake;
+    makeGuess(
+        player: Snowflake,
+        guess: string,
+    ): SpecialTurnResponse | CharState[];
 }
 
 export interface Colors {
-	wrong: string;
-	right: string;
-	wrongPosition: string;
+    wrong: string;
+    right: string;
+    wrongPosition: string;
 }
 
-export function resolve(charResult: CharState, colors: Colors): string {
-	switch (charResult) {
-		case CharState.Correct:
-			return colors.right;
-		case CharState.Moved:
-			return colors.wrongPosition;
-		case CharState.Wrong:
-		default:
-			return colors.wrong;
-	}
+export interface RenderDimensions {
+    imageSize: number;
+    marginBottom: number;
+    fontSize: number;
+    borderSize: number;
+}
+
+export class Small implements RenderDimensions {
+    imageSize: number = 50;
+    marginBottom: number = 12;
+    fontSize: number = 40;
+    borderSize: number = 1;
+}
+export class Default implements RenderDimensions {
+    imageSize: number = 200;
+    marginBottom: number = 36;
+    fontSize: number = 180;
+    borderSize: number = 2;
 }
 
 export class DefaultColors implements Colors {
-	wrong: string = "#FFFFFF"; // White
-	right: string = "#65E43C"; // Atlantis
-	wrongPosition: string = "#E6CD2E"; // Sunflower
+    wrong: string = "#FFFFFF"; // White
+    right: string = "#65E43C"; // Atlantis
+    wrongPosition: string = "#E6CD2E"; // Sunflower
+}
+
+export class RenderParameters {
+    colors: Colors = new DefaultColors();
+    dimensions: RenderDimensions = new Default();
+}
+
+export function resolve(charResult: CharState, colors: Colors): string {
+    switch (charResult) {
+        case CharState.Correct:
+            return colors.right;
+        case CharState.Moved:
+            return colors.wrongPosition;
+        case CharState.Wrong:
+        default:
+            return colors.wrong;
+    }
 }
 
 export interface Renderer {
-	render(word: string, guessResult: CharState[], colors?: Colors): Buffer;
+    render(
+        word: string,
+        guessResult: CharState[],
+        parameters?: RenderParameters,
+    ): Buffer;
 }
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,45 +1,39 @@
-import {
-	Renderer,
-	CharState,
-	Colors,
-	DefaultColors,
-	resolve,
-} from "./interfaces";
+import { Renderer, CharState, RenderParameters, resolve } from "./interfaces";
 
 import { UltimateTextToImage, HorizontalImage } from "ultimate-text-to-image";
 
 export class Basic implements Renderer {
-	render(
-		word: string,
-		guessResult: CharState[],
-		colors?: Colors
-	): Buffer {
-		const effectiveColors = colors || new DefaultColors();
-		const images: UltimateTextToImage[] = [];
-		[...word].forEach((character, index) => {
-			const backgroundColor = resolve(
-				guessResult[index],
-				effectiveColors
-			);
-			images.push(
-				new UltimateTextToImage(character, {
-					valign: "bottom",
-					align: "center",
-					marginBottom: 12,
-					width: 50,
-					height: 50,
-					backgroundColor: backgroundColor,
-					fontSize: 40,
-					borderSize: 1,
-					borderColor: "#000000",
-				})
-			);
-		});
-		return new HorizontalImage(images, {
-			valign: "middle",
-			margin: 0,
-		})
-			.render()
-			.toBuffer();
-	}
+    render(
+        word: string,
+        guessResult: CharState[],
+        parameters?: RenderParameters,
+    ): Buffer {
+        const effectiveParameters = parameters || new RenderParameters();
+        const images: UltimateTextToImage[] = [];
+        [...word].forEach((character, index) => {
+            const backgroundColor = resolve(
+                guessResult[index],
+                effectiveParameters.colors,
+            );
+            images.push(
+                new UltimateTextToImage(character, {
+                    valign: "bottom",
+                    align: "center",
+                    marginBottom: effectiveParameters.dimensions.marginBottom,
+                    width: effectiveParameters.dimensions.imageSize,
+                    height: effectiveParameters.dimensions.imageSize,
+                    backgroundColor: backgroundColor,
+                    fontSize: effectiveParameters.dimensions.fontSize,
+                    borderSize: effectiveParameters.dimensions.borderSize,
+                    borderColor: "#000000",
+                }),
+            );
+        });
+        return new HorizontalImage(images, {
+            valign: "middle",
+            margin: 0,
+        })
+            .render()
+            .toBuffer();
+    }
 }
