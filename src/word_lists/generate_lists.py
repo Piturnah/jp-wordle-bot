1a598ff4340c170293c6e1ad8eeb601f4f23c8f1
Piturnah <peterhebden6@gmail.com>
2022-02-02 04:32:00 +0000

    Check guesses are in word list

--- src/word_lists/generate_lists.py
+++ src/word_lists/generate_lists.py
@@ -9,9 +9,9 @@ def generate_lists(path, out):
     with open(path, "r", encoding="utf-8") as f:
         content = f.read()
     
-    four_kana = {}
-    five_kana = {}
-    six_kana = {}
+    four_kana = []
+    five_kana = []
+    six_kana = []
 
     word_lists = {
         4: four_kana,
@@ -22,8 +22,8 @@ def generate_lists(path, out):
     for x in json.loads(content)["words"]:
         word = kks.convert(x["kana"][0]["text"])[0]["hira"]
         if len(word) in word_lists.keys():
-            word_lists[len(word)][word] = { "eng": x["sense"][0]["gloss"][0]["text"],
-                                            "kanji": next(iter(x["kanji"]), {"text":""})["text"]}
+            word_lists[len(word)].append( [word,  { "eng": x["sense"][0]["gloss"][0]["text"],
+                                            "kanji": next(iter(x["kanji"]), {"text":""})["text"] } ] )
     
     with open(out, "w+", encoding="utf-8") as f:
         f.write(json.dumps(word_lists, indent=4, ensure_ascii=False))
