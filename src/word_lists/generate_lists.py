c0849828e4d6c34085486f3a8b5d66b3e9f03c1c
Peter Hebden <peterhebden6@gmail.com>
2022-02-02 03:41:24 +0000

    Update list generation method

--- src/word_lists/generate_lists.py
+++ src/word_lists/generate_lists.py
@@ -9,18 +9,21 @@ def generate_lists(path, out):
     with open(path, "r", encoding="utf-8") as f:
         content = f.read()
     
-    four_kana = []
-    five_kana = []
-    six_kana = []
+    four_kana = {}
+    five_kana = {}
+    six_kana = {}
 
     word_lists = {
         4: four_kana,
         5: five_kana,
         6: six_kana
     }
-    for word in map(lambda x: kks.convert(x["kana"][0]["text"])[0]["hira"], json.loads(content)["words"]):
+    
+    for x in json.loads(content)["words"]:
+        word = kks.convert(x["kana"][0]["text"])[0]["hira"]
         if len(word) in word_lists.keys():
-            word_lists[len(word)].append(word)
+            word_lists[len(word)][word] = { "eng": x["sense"][0]["gloss"][0]["text"],
+                                            "kanji": next(iter(x["kanji"]), {"text":""})["text"]}
     
     with open(out, "w+", encoding="utf-8") as f:
         f.write(json.dumps(word_lists, indent=4, ensure_ascii=False))
