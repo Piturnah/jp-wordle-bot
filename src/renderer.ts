0470f06d1be8a9b5fc425a9fd73e7b3cdaa09cd7
Janosch Reppnow <janoschre@gmail.com>
2022-02-19 21:31:38 +0100

    test commit for rendering stuff

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,8 +1,29 @@
-import { HorizontalImage, UltimateTextToImage } from "ultimate-text-to-image";
+import { Logger } from "tslog";
+import {
+    HorizontalImage,
+    UltimateTextToImage,
+    registerFont,
+} from "ultimate-text-to-image";
 
 import { CharResult, RenderParameters, Renderer, resolve } from "./interfaces";
 
 export class Basic implements Renderer {
+    private static readonly fontAlias = "customFont";
+    private readonly font?: string;
+    private readonly logger = new Logger();
+
+    constructor() {
+        const fontDir = "./fonts/NotoSansTC-Regular.otf";
+        if (undefined !== fontDir) {
+            this.logger.info(
+                "Trying to register custom font specified in .env:",
+                fontDir,
+            );
+            registerFont(fontDir, { family: Basic.fontAlias });
+            this.font = Basic.fontAlias;
+        }
+    }
+
     render(guessResult: CharResult[], parameters?: RenderParameters): Buffer {
         const params = parameters || new RenderParameters();
         const images: UltimateTextToImage[] = [];
@@ -10,6 +31,7 @@ export class Basic implements Renderer {
             const backgroundColor = resolve(result.result, params.colors);
             images.push(
                 new UltimateTextToImage(result.character, {
+                    fontFamily: this.font,
                     valign: "bottom",
                     align: "center",
                     marginBottom: params.dimensions.marginBottom,
